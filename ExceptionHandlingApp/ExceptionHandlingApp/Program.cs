fa3f60ecc397982938d4825047de891a220903b0
Kishore Kumar R <150365994+R-Kishore-Kumar-KK@users.noreply.github.com>
2026-03-30 23:09:24 +0530

    Exception Handling in C#

---
+++ ExceptionHandlingApp/ExceptionHandlingApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ExceptionHandlingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> {"Apple", "Banana", "Cherry" };
+
+            try
+            {
+                String fruit;
+                fruit = list[5];
+                Console.WriteLine("fruit: " + fruit);
+            }
+            catch (ArgumentOutOfRangeException ex) {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                int number;
+                Console.WriteLine("Enter a Number: ");
+                number = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
