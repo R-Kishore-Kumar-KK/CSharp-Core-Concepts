3639569399967eda59bc7c6f3f99d0cd470c2f66
Kishore Kumar R <150365994+R-Kishore-Kumar-KK@users.noreply.github.com>
2026-03-30 23:11:50 +0530

    Added C# Examples for Classes, Variable Types, Access Modifer, Modifier, Member Function Types,

---
+++ MyConsoleApplication/MyConsoleApplication/MyClass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyConsoleApplication
+{
+    public class MyClass
+    {
+        int empId;
+        String name;
+
+        public MyClass()
+        {
+            
+        }
+
+        public MyClass(int empId, String name) { 
+            this.empId = empId;
+            this.name = name;
+        }
+
+        public static void method1()
+        {
+
+        }
+
+        public static void method2(int a)
+        {
+            Console.WriteLine("The Number: " + a);
+        }
+
+        public static int method3()
+        {
+            int v = 0;
+            return v;
+        }
+
+        public static int method4(int m, int n) {
+            int sum = m + n;
+            return sum;
+        }
+
+        public int method5() {
+            return 0;
+        }
+
+        private String name1;
+        private int age;
+
+        public String Name1
+        {
+            get {return name1;}
+            set {name1 = value;}
+        }
+
+        public int Age
+        {
+            get{return age;}
+            set 
+            { 
+                if(value >= 18)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Age cannot be less than 18.");
+                }
+            }
+        }
+
+        public void dispalyInfo()
+        {
+            Console.WriteLine("Name: " + name1 + " Age: " + age);
+        }
+
+        public int stuId { get; set; }
+        public int age1 { get; set; }
+
+        public MyClass(int stuId, int age)
+        {
+            this.stuId = stuId;
+            this.age1 = age;
+        }
+
+        public void displayStuInfo()
+        {
+            Console.WriteLine("Student Id: " + stuId  + " Student Age: " + age1);
+        }
+    }
+}
---
+++ MyConsoleApplication/MyConsoleApplication/Program.cs
@@ -0,0 +1,56 @@
+﻿using MyConsoleApplication;
+
+MyClass myClass = new MyClass(10, "Kishore");
+
+MyClass.method1();
+
+int a;
+Console.WriteLine("Enter the Number: ");
+a = int.Parse(Console.ReadLine());
+MyClass.method2(a);
+
+int v = MyClass.method3();
+Console.WriteLine("Default: " +  v);
+
+int m, n;
+Console.Write("Enter a Number: ");
+m = int.Parse(Console.ReadLine());
+
+Console.Write("Enter a Number: ");
+n = int.Parse(Console.ReadLine());
+
+
+int sum = MyClass.method4(m, n);
+
+Console.WriteLine("Sum: "+ sum);
+
+MyClass myClass5 = new MyClass();
+int default5 = myClass.method5();
+
+Console.WriteLine("Default 5 : " + default5);
+
+MyClass myClass6 = new MyClass();
+myClass6.Name1 = "Test";
+myClass6.Age = 28;
+
+Console.WriteLine("Info: ");
+myClass6.dispalyInfo();
+
+int stuId;
+int stuAge;
+
+Console.WriteLine("Enter Student Id: ");
+stuId = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Student Age: ");
+stuAge = int.Parse(Console.ReadLine());
+
+MyClass myClass7  = new MyClass(stuId, stuAge);
+Console.WriteLine("Student Details:");
+myClass7.displayStuInfo();
+
+myClass7.stuId = 102;
+myClass7.age1 = 30;
+
+Console.WriteLine("Updated Student Details:");
+myClass7.displayStuInfo();
