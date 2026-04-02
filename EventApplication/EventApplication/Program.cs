36a6c758820ea6acbe056e7ed40bd603a2a76997
Kishore Kumar R <150365994+R-Kishore-Kumar-KK@users.noreply.github.com>
2026-04-03 01:33:07 +0530

    Added Events Application

---
+++ EventApplication/EventApplication/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+
+public class Program 
+{ 
+    static void OnProcessCompleted() 
+    { 
+        Console.WriteLine("Process Completed!");
+    }
+
+    static void SendEmail()
+    {
+        Console.WriteLine("Email sent to Customer");
+    }
+
+    static void SendSMS()
+    {
+        Console.WriteLine("SMS sent to Customer");
+    }
+
+    static void Main() 
+    {
+        Process process = new Process();
+
+        //Subscribe
+        process.ProcessCompleted += OnProcessCompleted;
+
+        process.StartProcess();
+
+        Order order = new Order();
+        order.OrderPlaced += SendEmail;
+        order.OrderPlaced += SendSMS;
+
+        order.PlaceOrder();
+    }
+}
+
+class Process
+{
+    public delegate void ProcessCompletedHandler();
+
+    //Event declaration
+    public event ProcessCompletedHandler ProcessCompleted;
+
+    public void StartProcess()
+    {
+        Console.WriteLine("Process Started");
+
+        //Raise Event
+        ProcessCompleted?.Invoke();
+    }
+}
+
+class Order
+{
+    public event Action OrderPlaced;
+
+    public void PlaceOrder()
+    {
+        Console.WriteLine("Order Placed Successfully");
+        OrderPlaced?.Invoke();
+    }
+}
